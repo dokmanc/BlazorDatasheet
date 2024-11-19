adb4c82ae5bfcdc914b5a3c444197e6c1976e89b
Chad Dokmanovich <chaddokmanovich@comcast.net>
2024-11-18 20:58:10 -0500

    Spreadsheet responds to browser resizing

---
+++ src/BlazorDatasheet.Server/wwwroot/custom.js
@@ -0,0 +1,20 @@
+﻿function setResizeHandler(dotNetHelper) {
+    function updateWidth() {
+        let width = window.innerWidth;
+
+        //const parentDiv = document.getElementById('datasheet-parent');
+        //parentDivWidth = parentDiv.clientWidth;
+
+        //console.log(`parentDiv.clientWidth: ${parentDiv.clientWidth}`);
+        //console.log(`window.innerWidth: ${window.innerWidth}`);
+
+        const sandwichMenu = document.getElementById('sandwich-menu');
+        const isSandwichMenuVisible = sandwichMenu && window.getComputedStyle(sandwichMenu).display !== 'none';
+
+
+        dotNetHelper.invokeMethodAsync('UpdateMaxWidth', width, isSandwichMenuVisible);
+    }
+
+    window.addEventListener('resize', updateWidth);
+    updateWidth(); // Set initial width
+}
